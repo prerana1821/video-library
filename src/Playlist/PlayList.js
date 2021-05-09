e55971fcaeeff2fd92125faf116797969d669d9b
prerana1821 <prerananawar1@gmail.com>
2021-05-09 13:23:46 +0530

    fix: show playlist

--- src/Playlist/PlayList.js
+++ src/Playlist/PlayList.js
@@ -37,30 +37,7 @@ export const PlayList = () => {
           return (
             <div key={playList.id}>
               <div className='playlist-info'>
-                {/* {editPlayListName && playList ? (
-                  <input
-                    type='text'
-                    value={setNewPlayListName}
-                    onChange={(e) => setNewPlayListName(e.target.value)}
-                  />
-                ) : ( */}
                 <h3>{playList.title}</h3>
-                {/* )} */}
-                <button
-                  className='btn-icon'
-                  // onClick={() => {
-                  //   console.log(playList);
-                  //   console.log(playListsArray.playList);
-                  //   playList === playListsArray[playList] &&
-                  //     setEditPlayListName(!editPlayListName);
-                  //   // playListDispatch({
-                  //   //   type: "EDIT_PLAYLIST_NAME",
-                  //   //   payload: { playList, newPlayListName },
-                  //   // });
-                  // }}
-                >
-                  <i className='fas fa-2x  fa-pen'></i>
-                </button>
                 <button
                   className='btn-icon'
                   onClick={() =>
@@ -74,39 +51,37 @@ export const PlayList = () => {
                 </button>
               </div>
               <div className='playlist-details'>
-                {likeSaveState.playlists.map((playList) => {
-                  return playList.videos.map((video) => {
-                    return (
-                      <Link to={`/video/${video.id}`}>
-                        <div key={video.id} className='card'>
-                          <img
-                            className='thumbnail'
-                            src={video.thumbnail}
-                            alt={video.name}
-                          />
-                          <h4>{video.name}</h4>
-                          <div className='card-actions'>
-                            <p>Category: {video.category}</p>
-                            <button
-                              className='btn-icon'
-                              onClick={(e) => {
-                                e.preventDefault();
-                                likeSaveDispatch({
-                                  type: "REMOVE_FROM_PLAYLIST",
-                                  payload: {
-                                    selectedPlayList: playList.title,
-                                    selectedVideo: video,
-                                  },
-                                });
-                              }}
-                            >
-                              <i className='fas fa-2x fa-trash-alt'></i>
-                            </button>
-                          </div>
+                {playList.videos.map((video) => {
+                  return (
+                    <Link to={`/video/${video.id}`}>
+                      <div key={video.id} className='card'>
+                        <img
+                          className='thumbnail'
+                          src={video.thumbnail}
+                          alt={video.name}
+                        />
+                        <h4>{video.name}</h4>
+                        <div className='card-actions'>
+                          <p>Category: {video.category}</p>
+                          <button
+                            className='btn-icon'
+                            onClick={(e) => {
+                              e.preventDefault();
+                              likeSaveDispatch({
+                                type: "REMOVE_FROM_PLAYLIST",
+                                payload: {
+                                  selectedPlayList: playList.title,
+                                  selectedVideo: video,
+                                },
+                              });
+                            }}
+                          >
+                            <i className='fas fa-2x fa-trash-alt'></i>
+                          </button>
                         </div>
-                      </Link>
-                    );
-                  });
+                      </div>
+                    </Link>
+                  );
                 })}
               </div>
             </div>
